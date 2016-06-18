e9ad14112f7e2a251aff30d42720fae9f39a6e1f
Nicholas Horton <nhort031@uottawa.ca>
2016-06-18 15:20:32 -0400

    Make instead array of menu options!

--- menu.rs
+++ menu.rs
@@ -1,24 +1,25 @@
 // Menu for a text-based adventure game.
 // (Also learn how to import/use code from another file!)
 
+#[derive(Clone)]
 struct MenuOption {
   selector: char,
   description: String,
 }
 
 pub fn display_menu() {
-  let option_new_game  : MenuOption = MenuOption { selector: 'n', description: "New Game".to_string(), };
-  let option_load_game : MenuOption = MenuOption { selector: 'l', description: "Load Game".to_string(), };
-  let option_quit      : MenuOption = MenuOption { selector: 'q', description: "Quit".to_string(), };
+  let options : [MenuOption; 3] = [MenuOption { selector: 'n', description: "New Game".to_string(), },
+                                   MenuOption { selector: 'l', description: "Load Game".to_string(), },
+                                   MenuOption { selector: 'q', description: "Quit".to_string(), }];
 
   println!("Welcome to The Great Rust Adventure!");
   println!("=-----------------------------------=\n");
   println!("  Menu  ");
   println!("--------");
 
-  display_menu_option(option_new_game);
-  display_menu_option(option_load_game);
-  display_menu_option(option_quit);
+  for i in 0..options.len() {
+    display_menu_option(options[i].clone());
+  }
 
 }
 
